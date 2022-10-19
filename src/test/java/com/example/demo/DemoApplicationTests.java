d15edefee6e8fdc47ea432f9798306a4dcbf4c2f
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-19 03:20:36 -0700

    up project

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.pos;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PosApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
